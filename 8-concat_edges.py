21dd5507b20aa70456a21caffac4ec0bfe3ad86e
lesliemoonsamy <lesliemoonsamy@outlook.com>
2023-12-02 10:10:51 -0800

    Updated

---
+++ 8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an  interactive, object-oriented programming language that
+combines power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
